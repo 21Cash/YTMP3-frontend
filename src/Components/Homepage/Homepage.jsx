5a712018a088661c6021e2176e002b0f11f1ccc4
21Cash <21ThousandProof@gmail.com>
2023-12-02 16:33:42 +0530

    Added Server Status

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,45 +1,138 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import InputBox from "../InputBox/InputBox";
+import { backendUrl } from "../../constants";
 
 const Homepage = () => {
+  const [serverStatus, setServerStatus] = useState(null);
+
   useEffect(() => {
-    // Set overflow hidden for the html and body elements
     document.documentElement.style.overflow = "hidden";
     document.body.style.overflow = "hidden";
 
-    // Cleanup function to restore overflow on component unmount
     return () => {
       document.documentElement.style.overflow = "visible";
       document.body.style.overflow = "visible";
     };
   }, []);
 
-  return (
-    <>
-      {/* New Navigation Bar */}
+  useEffect(() => {
+    const checkServerStatus = async () => {
+      try {
+        console.log("Pinging Server");
+        const response = await fetch(`${backendUrl}/test`);
+        if (response.ok) {
+          console.log("Ping Success");
+          setServerStatus("Online");
+        } else {
+          console.log("Ping Failed");
+          setServerStatus("Offline");
+        }
+      } catch (error) {
+        console.error("Error checking server status:", error);
+        setServerStatus("Offline");
+      }
+    };
+
+    checkServerStatus();
+  }, []);
+
+  const styles = {
+    title: {
+      fontSize: "2.5rem",
+      marginBottom: "20px",
+      textAlign: "center",
+      width: "100%",
+    },
+    navTitle: {
+      padding: "0px 0px 0px 10px",
+      fontSize: "2rem",
+      margin: 0,
+    },
+  };
+
+  const ServerStatus = ({ status }) => {
+    let statusColor = "#ff0000";
+
+    if (status === "Online") {
+      statusColor = "#00ff00";
+    }
+
+    return (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div
+          style={{
+            width: "10px",
+            height: "10px",
+            borderRadius: "50%",
+            backgroundColor: statusColor,
+            marginRight: "5px",
+          }}
+        ></div>
+        <p>Server: {status}</p>
+      </div>
+    );
+  };
+
+  const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
       <div
         style={{
-          backgroundColor: "#333", // Background color for the navigation bar
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          backgroundColor: "#333",
+          color: "#fff",
+          padding: "10px",
+          textAlign: "center",
+          boxSizing: "border-box",
+        }}
+      >
+        <p>
+          © {currentYear} Sushil L, No Rights Reserved |{" "}
+          <a
+            href="https://github.com/21Cash/YTMP3-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#4a90e2", textDecoration: "none" }}
+          >
+            View Source Code
+          </a>
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#333",
           color: "#4a90e2",
           padding: "10px",
           textAlign: "center",
           boxSizing: "border-box",
-          height: "60px", // Set the height of the navigation bar
+          height: "60px",
           position: "fixed",
           top: 0,
           left: 0,
           right: 0,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
         <h1 style={styles.navTitle}>21YTMP3</h1>
+        <ServerStatus status={serverStatus} />
       </div>
 
-      {/* Main Content */}
       <div
         style={{
           backgroundColor: "#1e1e1e",
-          height: "calc(100vh - 60px)", // Set height to 100vh minus the navigation bar height
-          overflow: "hidden", // Hide overflow to prevent scrolling
+          height: "calc(100vh - 60px)",
+          overflow: "hidden",
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
@@ -47,59 +140,15 @@ const Homepage = () => {
           color: "#4a90e2",
           padding: "20px",
           boxSizing: "border-box",
-          marginTop: "60px", // Add margin to prevent content from being covered by the fixed navigation bar
+          marginTop: "60px",
         }}
       >
-        <h1 style={styles.title}> Download MP3</h1> {/* Updated title header */}
+        <h1 style={styles.title}>Download MP3</h1>
         <InputBox />
       </div>
 
-      {/* Footer */}
       <Footer />
     </>
-  );
-};
-
-const styles = {
-  title: {
-    fontSize: "2.5rem",
-    marginBottom: "20px",
-  },
-  navTitle: {
-    fontSize: "2rem", // Adjusted font size for the navigation bar title
-    margin: 0,
-  },
-};
-
-const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
-  return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        width: "100%",
-        backgroundColor: "#333",
-        color: "#fff",
-        padding: "10px",
-        textAlign: "center",
-        boxSizing: "border-box",
-      }}
-    >
-      <p>
-        © {currentYear} Sushil L, No Rights Reserved |{" "}
-        <a
-          href="https://github.com/21Cash/YTMP3-frontend"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: "#4a90e2", textDecoration: "none" }}
-        >
-          View Source Code
-        </a>
-      </p>
-    </div>
   );
 };
 
